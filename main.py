1edaa5e6a10b29f0cdab905c96da2b5c9ad3ab72
Bartekoo <bartek.purta1@gmail.com>
2024-03-14 01:07:50 +0100

    added get_item_abilities function

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ def find_unique_name(name, tier, language, enchant):
     with open("all_items.json", 'r', encoding='utf-8') as file:
         all_items = json.load(file)
 
-    print(full_name)
-
     # Iterate through each item in the JSON data
     for item in all_items:
         # Check if the item has 'LocalizedNames' and if the English name matches
@@ -47,8 +45,6 @@ def get_item_icons(itemID):
 
     response = requests.get(url)
 
-    print(url)
-
     if response.status_code == 200:
         data = {f"{itemID}": url}
         with open("icon.json", "w") as json_file:
@@ -57,15 +53,51 @@ def get_item_icons(itemID):
     return None
 
 def get_item_abilities(itemName):
+    session = requests.Session()
     url = f"https://wiki.albiononline.com/wiki/{itemName}"
+    response = session.get(url)
 
-    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        tables = soup.find_all('table', class_="wikitable")
+        # Ignore the first table
+        tables = tables[1:]
+        abilities_list = []
+
+        for i, table in enumerate(tables, 1):
+            abilities_dict = {"slot": str(i), "abilities": {}}
+            trs = table.find_all("tr")
+            # Ignore first 3 rows of the table, but for the 5th table, ignore first 2 rows
+            if i == 4:
+                trs = trs[2:]
+            else:
+                trs = trs[3:]
+            
+            ability_index = 1  # Initialize ability index for each table
+            for j, tr in enumerate(trs, 1):
+                tds = tr.find_all("td", attrs={"rowspan": ["1", "2"]})
+                for td in tds:
+                    ability_text = td.text.strip()
+                    if ability_text:
+                        abilities_dict["abilities"][str(ability_index)] = ability_text
+                        ability_index += 1
+
+            abilities_list.append(abilities_dict)
+
+        with open("abilities.json", "w") as json_file:
+            json.dump(abilities_list, json_file, indent=4)
+
+    else:
+        print("Failed to fetch data from the URL:", url)
+
 @app.route('/<itemName>/<tier>/<enchants>/<quality>/<location>', methods=['GET'])  
 def get_items(itemName, tier, enchants, quality, location):
 
     itemID = find_unique_name(itemName, tier, "EN-US", enchants);
 
     result = searchitem(location, itemID, quality)
+
+    get_item_abilities(itemName)
 
     if result:
         return jsonify(result, get_item_icons(itemID))
